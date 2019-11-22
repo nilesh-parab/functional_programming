d803abab173867e94fc5465707bbe295317de26f
nilesh-parab <nilesh.parab@adapty.com>
2019-11-22 11:20:38 +0530

    currying & composition

---
+++ composition.js
@@ -0,0 +1,29 @@
+// curry function because each accepts 1 arguments
+const sum = a => b => a + b;
+
+// inc is a partial function because sum(1) returns function & not a value
+// also exhibits closures as we are initialising sum function with 1 set in the lexical scope
+const increment = sum(1);
+
+const product = a => b => a * b;
+
+const multiplyBy2 = product(2);
+
+const subtract = a => b => a - b;
+
+const decrement = subtract(1);
+
+// this function composes mulitiple functions using reduceRight function
+const compose = (...fns) => x => fns.reduceRight((accumlator, currValue) => currValue(accumlator), x)
+
+// partially applied compose function
+const opsComposed = compose(increment, multiplyBy2, decrement);
+
+console.log(opsComposed(2));
+
+console.log(decrement(2));
+
+// another example of composition
+const incAndMul = n => multiplyBy2(increment(n));
+
+console.log(incAndMul(10));
---
+++ currying.md
@@ -0,0 +1,14 @@
+currying
+	pointfree code
+	composability
+	partial functions
+	closures
+	
+Composition
+	Function composition is the process of combining two or more functions to produce a new function.
+	Put simply, a composition of functions `f` and `g` can be defined as `f(g(x))`, which evaluates from the inside out — right to left.
+	
+To Clear
+	bind
+	global scope
+	lexical scope closure
